e0e60b05830704072ee5840dfe3d47666fdd4487
deenahl0023 <94314653+deenahl0023@users.noreply.github.com>
2023-01-14 19:57:22 -0800

    remove script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Hides navigations bar after scrolling some time 
+// // Hides navigations bar after scrolling some time 
 
-function hideNavigation() {
-    var nav = document.getElementById("horizontal-menu");
+// function hideNavigation() {
+//     var nav = document.getElementById("horizontal-menu");
     
-    var scrollAmount = window.pageYOffset;
-    var elementVisible = 50;
+//     var scrollAmount = window.pageYOffset;
+//     var elementVisible = 50;
 
-    if (scrollAmount > elementVisible) {
-        nav.classList.remove("active");
-    } else {
-        nav.classList.add("active");
-    }
-}
+//     if (scrollAmount > elementVisible) {
+//         nav.classList.remove("active");
+//     } else {
+//         nav.classList.add("active");
+//     }
+// }
 
-window.addEventListener("scroll", hideNavigation);
+// window.addEventListener("scroll", hideNavigation);
