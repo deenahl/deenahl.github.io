6a4d0d30924bfd46316d11b737b729a6dd5bcc61
deenahl0023 <94314653+deenahl0023@users.noreply.github.com>
2023-01-14 19:48:01 -0800

    trying to add horizontal menu

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Hides navigations bar after scrolling some time 
 
-function hideNavigaation() {
+function hideNavigation() {
     var nav = document.getElementById("horizontal-menu");
     
-    
+    var scrollAmount = window.pageYOffset;
+    var elementVisible = 50;
+
+    if (scrollAmount > elementVisible) {
+        nav.classList.remove("active");
+    } else {
+        nav.classList.add("active");
+    }
 }
+
+window.addEventListener("scroll", hideNavigation);
